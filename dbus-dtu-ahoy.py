caec7c32f2d5dfb8d34127c5759eccc4e66eaace
Dirk Steinkopf <dirk@steinkopf.net>
2022-12-14 11:20:14 +0100

    update for new ahoy api (0.5.28+)

--- dbus-dtu-ahoy.py
+++ dbus-dtu-ahoy.py
@@ -49,14 +49,10 @@ class DbusDTUAHOYService:
 
         self._config = self._getConfig()
 
-        # get data from Senec
-        inverter_data = self._getDTU_AHOYData()
+        self._fetch_AHOYData()
  
         logging.debug("%s /DeviceInstance = %d" %
                       (servicename, deviceinstance))
-
-        # debug:
-        #logging.info("Senec serial: %s" % (self._getSenecSerial()))
 
         # Create the management objects, as specified in the ccgx dbus-api document
         self._dbusservice.add_path('/Mgmt/ProcessName', __file__)
@@ -67,7 +63,6 @@ class DbusDTUAHOYService:
         # Create the mandatory objects
         self._dbusservice.add_path('/DeviceInstance', deviceinstance)
         self._dbusservice.add_path('/ProductId', 789 )  ## Keine Ahnung was hier stehen muss!?!
-        #self._dbusservice.add_path('/DeviceType', 345)  ## Keine Ahnung was hier stehen muss!?!
         self._dbusservice.add_path('/ProductName', productname)
         self._dbusservice.add_path('/CustomName', productname)
         #self._dbusservice.add_path('/Latency', None)
@@ -79,7 +74,7 @@ class DbusDTUAHOYService:
         # DSTK_2022-10-24 Position was 1. Changed to 0
         self._dbusservice.add_path('/Position', 0)   ## 0=AC input 1; 1=AC output  PV-Wechselrichter ; 2=AC input 2 GENERATOR
         #self._dbusservice.add_path('/Serial', self._getDTU_AHOYSerial())
-        self._dbusservice.add_path('/Serial', 1)
+        self._dbusservice.add_path('/Serial', 2)
         self._dbusservice.add_path('/UpdateIndex', 0)
         self._dbusservice.add_path('/Ac/NumberOfPhases', 1)
         self._dbusservice.add_path('/Ac/NumberOfAcInputs', 1)
@@ -98,20 +93,7 @@ class DbusDTUAHOYService:
         gobject.timeout_add(1000, self._update)
 
         # add _signOfLife 'timer' to get feedback in log every 5minutes
-        gobject.timeout_add(self._getSignOfLifeInterval()
-                            * 60*1000, self._signOfLife)
-
-    def _getDTU_AHOYSerial(self):
-        inverter_data = self._getDTU_AHOYData()
-
-        device_id = next((x for x in inverter_data if x['id'] == self._getDTU_AHOYDeviceId()), None)
-
-        if not device_id[0]:
-            raise ValueError(
-                "Response does not contain 'DEVICE_ID' attribute")
-
-        serial = device_id[0] 
-        return serial
+        gobject.timeout_add(self._getSignOfLifeInterval() * 60*1000, self._signOfLife)
 
     def _getConfig(self):
         config = configparser.ConfigParser()
@@ -131,98 +113,35 @@ class DbusDTUAHOYService:
         value = self._config['DEFAULT']['DTU_AHOY_DEVICENAME']
         return value        
 
-    def _getDTU_AHOYDeviceId(self):
-        value = self._config['DEFAULT']['DTU_AHOY_ID']
-        return value
-
-    def _getDTU_AHOY_U_AC(self):
-        value = self._config['DEFAULT']['DTU_AHOY_CH0_U_AC']
-        return value
-
-    def _getDTU_AHOY_I_AC(self):
-        value = self._config['DEFAULT']['DTU_AHOY_CH0_I_AC']
-        return value
-        
-    def _getDTU_AHOY_P_AC(self):
-        value = self._config['DEFAULT']['DTU_AHOY_CH0_P_AC']
-        return value
-
-    def _getDTU_AHOY_FREQ(self):
-        value = self._config['DEFAULT']['DTU_AHOY_CH0_FREQ']
-        return value
-
-    def _getDTU_AHOY_CH1_U_DC(self):
-        value = self._config['DEFAULT']['DTU_AHOY_CH1_U_DC']
-        return value
-
-    def _getDTU_AHOY_CH1_P_DC(self):
-        value = self._config['DEFAULT']['DTU_AHOY_CH1_P_DC']
-        return value
-        
-    def _getDTU_AHOY_CH1_I_DC(self):
-        value = self._config['DEFAULT']['DTU_AHOY_CH1_I_DC']        
-        return value
-
-    def _getDTU_AHOY_CH2_U_DC(self):
-        value = self._config['DEFAULT']['DTU_AHOY_CH2_U_DC']
-        return value
-
-    def _getDTU_AHOY_CH2_P_DC(self):
-        value = self._config['DEFAULT']['DTU_AHOY_CH2_P_DC']
-        return value
-        
-    def _getDTU_AHOY_CH2_I_DC(self):
-        value = self._config['DEFAULT']['DTU_AHOY_CH2_I_DC']
-        return value
-
-    def _getDTU_AHOY_YIELDDAY(self):
-        value = self._config['DEFAULT']['DTU_AHOY_CH0_YIELDDAY']
-        return value
-
-    def _getDTU_AHOY_YIELDTOTAL(self):
-        value = self._config['DEFAULT']['DTU_AHOY_CH0_YIELDTOTAL']
-        return value
-
-    def _getDTU_AHOY_LAST_MSG(self):
-        value = self._config['DEFAULT']['DTU_AHOY_LAST_MSG']
-        return value
-
     def _getDTU_AHOY_Path(self):
         value = self._config['DEFAULT']['DTU_AHOY_HOSTPATH']
         return value
 
-    def _getDTU_AHOYData(self):
-        URL = self._getDTU_AHOY_Path() + "/json" 
-
-        headers = {}
-
-        inverter = requests.request("GET", URL, headers=headers)
+    def _fetch_AHOYData(self):
+        URL = self._getDTU_AHOY_Path() + "/api/live" 
+        inverter = requests.request("GET", URL)
 
         # check for response
         if not inverter:
             raise ConnectionError("No response from AHOY_DTU - %s" % (URL))
 
-        inverter_data_x = inverter.json()
-        ##inverter_data = inverter_data_x['HM600']
-        
+        live_data = inverter.json()
         devicename = self._getDTU_AHOY_DEVICENAME()
-        #print( devicename )
-        inverter_data = inverter_data_x[devicename]
         
-       
-        ##for k in inverter_data:
-        ##    print(k)
+        all_inverters = live_data['inverter']
+        self._inverter_data = list(filter(lambda arr: arr['name'] == devicename, all_inverters))[0]
+        ac_data_index = self._inverter_data['ch_names'].index('AC')
+        self._ac_data = self._inverter_data['ch'][ac_data_index]
         
+        self._ac_data_field_names = live_data['ch0_fld_names']
+
         # check for Json
-        if not inverter_data:
+        if not self._inverter_data:
             raise ValueError("Converting response to JSON failed")
-
-        return inverter_data
-
-    def _floatFromHex(self, val):
-
-        return struct.unpack('!f', bytes.fromhex(val[3:]))[0]
-        #struct.unpack('!f', (val[3:]).decode('hex'))[0]
+    
+    def _getFieldByName(self, fieldname):
+        index = self._ac_data_field_names.index(fieldname)
+        return self._ac_data[index]
 
     def _signOfLife(self):
         logging.info("--- Start: sign of life ---")
@@ -233,55 +152,28 @@ class DbusDTUAHOYService:
 
     def _update(self):
         try:
-            # get data from DTU_AHOY
-            inverter_data = self._getDTU_AHOYData()
-
+            self._fetch_AHOYData()
+            
             # send data to DBus
-            last_msg = (inverter_data)[self._getDTU_AHOY_LAST_MSG()]
-      
-            total_voltage = (inverter_data)[self._getDTU_AHOY_U_AC()][0]
-            total_current = (inverter_data)[self._getDTU_AHOY_I_AC()][0]
-            total_power = (inverter_data)[self._getDTU_AHOY_P_AC()][0]
-            total_frequency = (inverter_data)[self._getDTU_AHOY_FREQ()][0]
-            
-            #total_ch0_voltage = (inverter_data)[self._getDTU_AHOY_CH1_U_DC()][0]
-            #total_ch0_power = (inverter_data)[self._getDTU_AHOY_CH1_P_DC()][0]
-            #total_ch0_currnet = (inverter_data)[self._getDTU_AHOY_CH1_I_DC()][0]
-            
-            #total_ch1_voltage = (inverter_data)[self._getDTU_AHOY_CH2_U_DC()][0]
-            #total_ch1_power = (inverter_data)[self._getDTU_AHOY_CH2_P_DC()][0]
-            #total_ch1_currnet = (inverter_data)[self._getDTU_AHOY_CH2_I_DC()][0]
-             
-             
-            total_yieldpower = (inverter_data)[self._getDTU_AHOY_YIELDDAY()][0] # day
-            total_yieldtotalpower = (inverter_data)[self._getDTU_AHOY_YIELDTOTAL()][0]
-        
+
              # positive: consumption, negative: feed into grid
-            self._dbusservice['/Ac/Energy/Forward'] = total_yieldpower / 1000
-            self._dbusservice['/Ac/Power'] = total_power
+            self._dbusservice['/Ac/Energy/Forward'] = self._getFieldByName('YieldDay') / 1000 # YieldDay = W
+            self._dbusservice['/Ac/Power'] = self._getFieldByName('P_AC')
             self._dbusservice['/ErrorCode'] = 0
             
             self._dbusservice['/Ac/MaxPower'] = 300
-            # self._dbusservice['/Ac/PowerLimit'] = 300 # this makes Multiplus change its Zero-Injection behaviour
- 
-            
-            #self._dbusservice['/Ac/L1/Energy/Forward'] = 0
-            #self._dbusservice['/Ac/L1/Voltage'] = 0
-            #self._dbusservice['/Ac/L1/Current'] = 0 
-            #self._dbusservice['/Ac/L1/Power'] = 0
-            
-            self._dbusservice['/Ac/L2/Energy/Forward'] =  total_yieldpower / 1000 # day
-            self._dbusservice['/Ac/L2/Voltage'] = total_voltage
-            self._dbusservice['/Ac/L2/Current'] = total_current
-            self._dbusservice['/Ac/L2/Power'] = total_power
-                       
-            #self._dbusservice['/Ac/L3/Energy/Forward'] = 0
-            #self._dbusservice['/Ac/L3/Voltage'] = 0
-            #self._dbusservice['/Ac/L3/Current'] = 0
-            #self._dbusservice['/Ac/L3/Power'] = 0
+            # self._dbusservice['/Ac/PowerLimit'] = 300 # this makes Multiplus change its zero injection behaviour
 
-            # logging.info("total_yieldpower: %s" % (total_yieldpower))
-            # logging.info("total_yieldpower = %s kWh, total_power: %s W" % (total_yieldpower / 1000, total_power))
+            # TODO: make L2 configurable            
+            self._dbusservice['/Ac/L2/Energy/Forward'] =  self._getFieldByName('YieldDay') / 1000 # YieldDay = W
+            self._dbusservice['/Ac/L2/Voltage'] = self._getFieldByName('U_AC')
+            self._dbusservice['/Ac/L2/Current'] = self._getFieldByName('I_AC')
+            self._dbusservice['/Ac/L2/Power'] = self._getFieldByName('P_AC')
+                       
+            #logging.info("voltage = %s" % self._getFieldByName('U_AC'))                    
+            #logging.info("YieldDay = %s kWh, P_AC: %s W" % (
+            #    self._getFieldByName('YieldDay') / 1000, 
+            #    self._getFieldByName('P_AC')))
 
             # increment UpdateIndex - to show that new data is available
             index = self._dbusservice['/UpdateIndex'] + 1  # increment index
@@ -333,31 +225,18 @@ def main():
             servicename='com.victronenergy.pvinverter',
             deviceinstance=77,
             
-
-            
             paths={
+                # We should not send 0 as initial value - this might just be wrong during normal operation (daylight)
                 '/Ac/Energy/Forward': {'initial': None, 'textformat': _kwh},
-                '/Ac/Power': {'initial': 0, 'textformat': _w},
+                '/Ac/Power': {'initial': None, 'textformat': _w},
                 '/ErrorCode': {'initial': 0, 'textformat': _w},
-                '/Ac/MaxPower': {'initial': 0, 'textformat': _w},
+                '/Ac/MaxPower': {'initial': 300, 'textformat': _w},
                 #no '/Ac/PowerLimit': {'initial': 300, 'textformat': _w},
-                
-				 
-                #'/Ac/L1/Energy/Forward': {'initial': None, 'textformat': _kwh},
-                #'/Ac/L1/Voltage': {'initial': 0, 'textformat': _v},
-                #'/Ac/L1/Current': {'initial': 0, 'textformat': _a},
-                #'/Ac/L1/Power': {'initial': 0, 'textformat': _w},				 
-     
+                				 
                 '/Ac/L2/Energy/Forward': {'initial': None, 'textformat': _kwh},
-                '/Ac/L2/Voltage': {'initial': 0, 'textformat': _v},
-                '/Ac/L2/Current': {'initial': 0, 'textformat': _a},
-                '/Ac/L2/Power': {'initial': 0, 'textformat': _w},	
-                
-                #'/Ac/L3/Energy/Forward': {'initial': None, 'textformat': _kwh},
-                #'/Ac/L3/Voltage': {'initial': 0, 'textformat': _v},
-                #'/Ac/L3/Current': {'initial': 0, 'textformat': _a},
-                #'/Ac/L3/Power': {'initial': 0, 'textformat': _w},	
-
+                '/Ac/L2/Voltage': {'initial': None, 'textformat': _v},
+                '/Ac/L2/Current': {'initial': None, 'textformat': _a},
+                '/Ac/L2/Power': {'initial': None, 'textformat': _w},	
             })
 
         logging.info(
@@ -370,3 +249,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+    
